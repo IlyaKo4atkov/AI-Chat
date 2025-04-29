911a411c74bae4b9aa65e4cbe2038bc6eebd07f5
Илья <ko4atkov.ilya@yandex.ru>
2025-04-29 18:10:13 +0300

    notifications.py

--- src/utils/notifications.py
+++ src/utils/notifications.py
@@ -1,11 +1,11 @@
-import os
-import logging
-from pathlib import Path
-from dotenv import load_dotenv
-from aiogram import Bot
-from aiogram.enums import ParseMode
-from aiogram.client.default import DefaultBotProperties
-import asyncio
+import os                                                  # Библиотека для работы с операционной системой
+import logging                                             # Библиотека для логирования
+from pathlib import Path                                   # Библиотека для работы с файловой системой
+from dotenv import load_dotenv                             # Библиотека для загрузки переменных окружения
+from aiogram import Bot                                    # Библиотека для взаимодействия бота Telegram
+from aiogram.enums import ParseMode                        # Парсинг текстовых сообщений
+from aiogram.client.default import DefaultBotProperties    # Создание экзепляра бота
+import asyncio                                             # Библиотека асинхронного программирования
 
 env_path = Path(__file__).parent.parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -20,7 +20,7 @@ async def send_telegram_message(
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
     if not bot_token or not chat_id:
-        logging.error("Telegram credentials not configured!")
+        logging.error("Учетные данные Telegram не настроены!")
         return False
 
     try:
